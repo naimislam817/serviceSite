a65540bb79113450cf308c44fbeb265326c7c4e8
naimislam817 <naimislam231@gmail.com>
2023-12-17 13:11:59 +0600

    Update Admin Dashboard Information

--- admin-dashboard.js
+++ admin-dashboard.js
@@ -1,55 +1,43 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // This function would typically make AJAX calls to retrieve data
-    // For demonstration, let's just create some placeholder content
-    loadDetails('driver-details', 'Driver details not available');
-    loadDetails('mechanic-details', 'Mechanic details not available');
-    loadDetails('booking-details', 'Booking details not available');
-    
-    loadSideTrayData('workshop-details', 'Workshop details not available');
-    loadSideTrayData('driver-history', 'Driver history not available');
-
-    createBookingChart(); // Function to create a booking services chart
+// Simulating a logout process
+document.getElementById('logout').addEventListener('click', function() {
+    // Here you would typically invalidate the user's session on the server.
+    alert('You have been logged out.');
+    // Redirect to the login page
+    window.location.href = 'index.html'; // Replace with the actual login page URL
 });
 
-function loadSideTrayData(containerId, placeholderText) {
-    const container = document.getElementById(containerId);
-    container.textContent = placeholderText;
-    // In a real application, you would retrieve and display data here
-}
-
-function createBookingChart() {
-    // You can use a chart library like Chart.js to create a chart
+// Function to generate a bar chart for booking services
+function generateBookingChart() {
     const ctx = document.getElementById('booking-chart').getContext('2d');
-    // Define your chart data and options here
-    // Example:
-    const chartData = {
-        labels: ['Service 1', 'Service 2', 'Service 3'],
-        datasets: [{
-            label: 'Booking Services',
-            data: [10, 20, 15], // Replace with actual data
-            backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)', 'rgba(255, 159, 64, 0.2)'],
-            borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)', 'rgba(255, 159, 64, 1)'],
-            borderWidth: 1
-        }]
-    };
-    
-    const chartOptions = {
-        scales: {
-            y: {
-                beginAtZero: true
+    const bookingChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Service A', 'Service B', 'Service C'],
+            datasets: [{
+                label: 'Number of Bookings',
+                data: [12, 19, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    };
-
-    new Chart(ctx, {
-        type: 'bar',
-        data: chartData,
-        options: chartOptions
     });
 }
 
-function loadDetails(containerId, placeholderText) {
-    const container = document.getElementById(containerId);
-    container.textContent = placeholderText;
-    // In a real application, you would retrieve and display data here
-}
+// Call the function to generate the chart when the script loads
+generateBookingChart();
